d29405a7393f3e43c002cafc6a8d4690a76da299
zmmohibul <18-36427-1@student.aiub.edu>
2022-02-06 17:51:15 +0600

    Data Structures - IntList - 3

--- IntList/src/IntList.java
+++ IntList/src/IntList.java
@@ -7,9 +7,24 @@ public class IntList {
         this.rest = r;
     }
 
+    public int size(IntList curr) {
+        if (curr.rest == null) {
+            return 1;
+        }
+
+        return size(curr.rest) + 1;
+    }
+
+    public int size() {
+        return this.size(this);
+    }
+
     public static void main(String[] args) {
         IntList L = new IntList(5, null);
         L.rest = new IntList(10, null);
         L.rest.rest = new IntList(15, null);
+        L.rest.rest.rest = new IntList(20, null);
+        L.rest.rest.rest.rest = new IntList(25, null);
+        System.out.println(L.size());
     }
 }
