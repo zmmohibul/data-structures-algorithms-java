03a93b818c7d0034008e702655e84b03a9d07e21
zmmohibul <18-36427-1@student.aiub.edu>
2022-02-08 16:39:24 +0600

    SLList - Cleaned

--- SLList/src/SLList.java
+++ SLList/src/SLList.java
@@ -1,95 +1,68 @@
 public class SLList {
     private IntNode first;
+    private int size;
+
+    private class IntNode {
+        public int item;
+        public IntNode next;
+
+        public IntNode(int item, IntNode next) {
+            this.item = item;
+            this.next = next;
+        }
+    }
 
     public SLList(int x) {
+        this.size += 1;
         this.first = new IntNode(x, null);
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     public void addFirst(int x) {
-        IntNode n = new IntNode(x, this.first);
+        this.size += 1;
+        IntNode n = new IntNode(x, first);
         this.first = n;
     }
 
     public int getFirst() {
-        return first.item;
-    }
-
-    private IntNode getLastNode(IntNode n) {
-        if (n.next == null) {
-            return n;
-        }
-
-        return getLastNode(n.next);
+        return this.first.item;
     }
 
     public void addLast(int x) {
-        if (first.next == null) {
+        this.size += 1;
+        addLast(x, first);
+    }
+
+    private void addLast(int x, IntNode p) {
+        if (p.next == null) {
             IntNode n = new IntNode(x, null);
-            first.next = n;
+            p.next = n;
             return;
         }
 
-        IntNode lastNode = getLastNode(first);
-        IntNode n = new IntNode(x, null);
-        lastNode.next = n;
-
+        addLast(x, p.next);
     }
 
-    public int getLast() {
-        IntNode n = getLastNode(first);
-        return n.item;
+    public int size() {
+        return this.size;
     }
 
-    private int getSize(IntNode n) {
-        if (n.next == null) {
+    /* public int size() {
+        return size(first);
+    }
+
+    private int size(IntNode p) {
+        if (p.next == null) {
             return 1;
         }
 
-        return 1 + getSize(n.next);
-    }
-
-    public int getSize() {
-        IntNode n = first;
-        return getSize(n);
-    }
+        return 1 + size(p.next);
+    } */
 
     public static void main(String[] args) {
         SLList L = new SLList(10);
-//        System.out.println(L.getFirst());
-        L.addFirst(15);
-//        System.out.println(L.getFirst());
-        L.addFirst(20);
-//        System.out.println(L.getFirst());
-        L.addFirst(25);
+        L.addFirst(5);
+        L.addLast(15);
         System.out.println(L.getFirst());
-        L.addLast(5);
-        System.out.println(L.getLast());
-        System.out.println(L.getSize());
+        System.out.println(L.size());
     }
 }
