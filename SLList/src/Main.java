de26ffae49e83e76c3e399846d47742f3a67d8d2
zmmohibul <18-36427-1@student.aiub.edu>
2022-02-08 18:25:51 +0600

    SLList - deleteFirst

--- SLList/src/Main.java
+++ SLList/src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         SLList L = new SLList(10);
         L.addFirst(5);
         L.addLast(15);
+//        L.deleteFirst();
         System.out.println(L.getFirst());
         System.out.println(L.size());
     }
--- SLList/src/SLList.java
+++ SLList/src/SLList.java
@@ -24,9 +24,23 @@ public class SLList {
         this.sentinal.next = new IntNode(x, null);
     }
 
+    public SLList(int[] arr) {
+        this.size = 0;
+        this.sentinal = new IntNode(11, null);
+        IntNode p = this.sentinal;
+        for (int i = 0; i < arr.length; i++) {
+            this.size += 1;
+            
+            IntNode n = new IntNode(arr[i], null);
+            p.next = n;
+            p = p.next;
+        }
+    }
+
     public void addFirst(int x) {
         this.size += 1;
         IntNode n = new IntNode(x, first);
+        n.next = this.sentinal.next;
         this.sentinal.next = n;
     }
 
@@ -51,5 +65,23 @@ public class SLList {
 
     public int size() {
         return this.size;
+    }
+
+    public void deleteFirst() {
+        this.sentinal.next = this.sentinal.next.next;
+        this.size -= 1;
+    }
+
+    public static void main(String[] args) {
+//        SLList L = new SLList(10);
+//        L.addFirst(5);
+//        L.addLast(15);
+//        L.deleteFirst();
+//        System.out.println(L.getFirst());
+//        System.out.println(L.size());
+
+        int arr[] = new int[] {1, 2, 3};
+
+        SLList L = new SLList(arr);
     }
 }
