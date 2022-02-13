ff2720aa9f4e55efefc2024af87a5e2ce78985f4
zmmohibul <18-36427-1@student.aiub.edu>
2022-02-14 00:05:33 +0600

    ArrayDeque - resize

--- ArrayDeque/src/ArrayDeque.java
+++ ArrayDeque/src/ArrayDeque.java
@@ -15,7 +15,7 @@ public class ArrayDeque<T> {
 
     public void addFirst(T item) {
         if (items[nextFirst] != null) {
-            //resize
+            resize();
         } else {
             items[nextFirst] = item;
             if (nextFirst - 1 == -1) {
@@ -28,7 +28,7 @@ public class ArrayDeque<T> {
 
     public void addLast(T item) {
         if (items[nextLast] != null) {
-            //resize
+            resize();
         } else {
             items[nextLast] = item;
             if (nextLast + 1 == size) {
@@ -39,14 +39,57 @@ public class ArrayDeque<T> {
         }
     }
 
+    public void resize() {
+        T[] newItems = (T[]) new Object[size * 2];
+        int newFirst = size;
+        int newLast = size + 1;
+
+        if (nextFirst < first) {
+            int numberOfItemsToCopy = first - nextFirst;
+            System.arraycopy(items, nextFirst + 1, newItems, newFirst - numberOfItemsToCopy + 1, numberOfItemsToCopy);
+            newFirst -= numberOfItemsToCopy;
+        } else {
+            System.arraycopy(items, 0, newItems, newFirst - first, first + 1);
+            newFirst = newFirst - first - 1;
+            if (nextFirst + 1 == size) {
+                // do nothing
+            } else {
+                int numberOfItemsToCopy = size - nextFirst - 1;
+                System.arraycopy(items, nextFirst + 1, newItems, newFirst - numberOfItemsToCopy + 1, numberOfItemsToCopy);
+                newFirst -= numberOfItemsToCopy;
+            }
+        }
+
+        if (last < nextLast) {
+            System.arraycopy(items, last, newItems, newLast, nextLast - last);
+        } else {
+            int numberOfItemsToCopy = size - last;
+            System.arraycopy(items, last, newItems, newLast, numberOfItemsToCopy);
+            newLast += numberOfItemsToCopy;
+            if (nextLast > 0) {
+                System.arraycopy(items, 0, newItems, newLast, nextLast);
+                newLast += nextLast;
+            }
+        }
+
+        first = size;
+        last = size + 1;
+        nextFirst = newFirst;
+        nextLast = newLast;
+        items = newItems;
+        size = size * 2;
+    }
+
     public static void main(String[] args) {
         ArrayDeque<Integer> arr = new ArrayDeque<>();
         arr.addFirst(11);
         arr.addFirst(11);
         arr.addFirst(11);
+        arr.addLast(12);
+        arr.addLast(12);
+        arr.addLast(12);
         arr.addFirst(11);
-        arr.addLast(12);
-        arr.addLast(12);
+        arr.addFirst(11);
         arr.addFirst(11);
     }
 }
