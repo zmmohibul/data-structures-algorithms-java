a387aa131e642caed98def24df1bf8eded34b2ea
zmmohibul <18-36427-1@student.aiub.edu>
2022-02-13 22:59:18 +0600

    ArrayDeque - addFirst, addLast

--- ArrayDeque/src/ArrayDeque.java
+++ ArrayDeque/src/ArrayDeque.java
@@ -14,9 +14,11 @@ public class ArrayDeque<T> {
     }
 
     public void addFirst(T item) {
-        if (items[nextFirst] == null) {
+        if (items[nextFirst] != null) {
+            //resize
+        } else {
             items[nextFirst] = item;
-            if (nextFirst == 0) {
+            if (nextFirst - 1 == -1) {
                 nextFirst = size - 1;
             } else {
                 nextFirst -= 1;
@@ -25,9 +27,11 @@ public class ArrayDeque<T> {
     }
 
     public void addLast(T item) {
-        if (items[nextLast] == null) {
+        if (items[nextLast] != null) {
+            //resize
+        } else {
             items[nextLast] = item;
-            if (nextLast == size - 1) {
+            if (nextLast + 1 == size) {
                 nextLast = 0;
             } else {
                 nextLast += 1;
@@ -35,48 +39,14 @@ public class ArrayDeque<T> {
         }
     }
 
-    private T[] resize() {
-        T[] arr = (T[]) new Object[size * 2];
-        int newFirst = size;
-        int newLast = size + 1;
-        if (items[last] != null) {
-            if (nextLast > first && nextLast <= size - 1) {
-                System.arraycopy(items, last, arr, newLast, size - last - 1);
-                newLast +=  size - last - 1;
-            } else {
-                System.arraycopy(items, last, arr, newLast, size - last);
-                newLast +=  size - last;
-            }
-
-            if (nextLast < first) {
-                System.arraycopy(items,0, arr, newLast, nextLast);
-                newLast += nextLast;
-            }
-        }
-
-        if (items[first] != null) {
-            if (nextFirst < first) {
-                int numberOfItemsToCopy = first - nextFirst;
-                int startPosition = newFirst - numberOfItemsToCopy + 1;
-                System.arraycopy(items, nextFirst + 1, arr, startPosition, numberOfItemsToCopy);
-                newFirst -= numberOfItemsToCopy;
-            } else {
-                int startPosition = newFirst - first;
-                System.arraycopy(items, 0, arr, startPosition, first + 1);
-            }
-        }
-
-        return arr;
-    }
-
     public static void main(String[] args) {
-        ArrayDeque<Integer> ad = new ArrayDeque<>();
-        ad.addFirst(11);
-        ad.addFirst(31);
-        ad.addFirst(33);
-        ad.addFirst(12);
-        ad.addFirst(21);
-        ad.addLast(44);
-        Integer[] a = ad.resize();
+        ArrayDeque<Integer> arr = new ArrayDeque<>();
+        arr.addFirst(11);
+        arr.addFirst(11);
+        arr.addFirst(11);
+        arr.addFirst(11);
+        arr.addLast(12);
+        arr.addLast(12);
+        arr.addFirst(11);
     }
 }
