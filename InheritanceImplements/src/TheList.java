2530df552191621097d3d253205c58ba0781c9e9
zmmohibul <18-36427-1@student.aiub.edu>
2022-02-20 10:06:04 +0600

    InheritanceImplements - TheList Interface

---
+++ InheritanceImplements/src/TheList.java
@@ -0,0 +1,24 @@
+public interface TheList<T> {
+    public void insert(T item, int position);
+
+    /** Adds x to the front of the list. */
+    public void addFirst(T x);
+
+    /** Adds x to the end of the list. */
+    public void addLast(T x);
+
+    /** Returns the first item in the list. */
+    public T getFirst();
+
+    /** Returns last item */
+    public T getLast();
+
+    /** Returns the ith item in the list. */
+    public T get(int i);
+
+    /** Returns the size of the list. */
+    public int size();
+
+    /** Deletes and returns last item. */
+    public T removeLast();
+}
