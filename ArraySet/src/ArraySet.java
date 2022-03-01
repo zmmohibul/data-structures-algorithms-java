b0e0284703ef967741e6769bab39bd7556fb8178
zmmohibul <18-36427-1@student.aiub.edu>
2022-03-01 13:48:59 +0600

    ArraySet

---
+++ ArraySet/src/ArraySet.java
@@ -0,0 +1,46 @@
+
+public class ArraySet<T> {
+    private T[] items;
+    private int size;
+
+
+    public ArraySet() {
+        this.items = (T[]) new Object[100];
+        size = 0;
+    }
+
+    public boolean contains(T x) {
+        if (size == 0) {
+            return false;
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (items[i].equals(x)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void add(T x) {
+        if (!contains(x)) {
+            items[size] = x;
+        }
+
+        size += 1;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public static void main(String[] args) {
+        ArraySet<String> s = new ArraySet<>();
+        s.add("horse");
+        s.add("fish");
+        s.add("house");
+        s.add("fish");
+        System.out.println(s.contains("horse"));
+        System.out.println(s.size());
+    }
+}
