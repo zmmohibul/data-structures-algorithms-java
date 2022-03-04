15ae378a4f4b59c247499b423b2cc839aea13b62
zmmohibul <18-36427-1@student.aiub.edu>
2022-03-04 17:10:14 +0600

    DS --- SinglyLinkedList - toString()

--- data-structures/SinglyLinkedList/src/SinglyLinkedList.java
+++ data-structures/SinglyLinkedList/src/SinglyLinkedList.java
@@ -22,11 +22,49 @@ public class SinglyLinkedList<T> {
         sentinel.next = n;
     }
 
+    public void addLast(T item) {
+        Node lastNode = getLastNode();
+        lastNode.next = new Node(item, null);
+    }
+
+    private Node getLastNode() {
+        return getLastNodeRecursive(sentinel.next);
+    }
+
+    private Node getLastNodeRecursive(Node ptr) {
+        if (ptr.next == null) {
+            return ptr;
+        }
+
+        return getLastNodeRecursive(ptr.next);
+    }
+
+    @Override
+    public String toString() {
+        Node ptr = sentinel.next;
+        StringBuilder sb = new StringBuilder("{ ");
+
+        while (ptr.next != null) {
+            sb.append(ptr.item);
+            sb.append(" -> ");
+            ptr = ptr.next;
+        }
+
+        sb.append(ptr.item);
+        sb.append(" }");
+
+       return sb.toString();
+    }
+
     public static void  main(String[] args) {
         SinglyLinkedList<Integer> singlyLinkedList = new SinglyLinkedList<>();
         singlyLinkedList.addFirst(11);
         singlyLinkedList.addFirst(9);
         singlyLinkedList.addFirst(6);
         singlyLinkedList.addFirst(3);
+        singlyLinkedList.addLast(14);
+        singlyLinkedList.addLast(17);
+        singlyLinkedList.addLast(19);
+        System.out.println(singlyLinkedList);
     }
 }
