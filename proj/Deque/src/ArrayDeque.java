5a82c3008c86109858ff6c71fc386e514f1f59a6
zmmohibul <18-36427-1@student.aiub.edu>
2022-03-04 19:26:45 +0600

    Proj --- ArrayDeque - resize-1

--- proj/Deque/src/ArrayDeque.java
+++ proj/Deque/src/ArrayDeque.java
@@ -1,41 +1,76 @@
 public class ArrayDeque<T> {
     private T[] items;
     private int size;
+    private int nextFirst;
+    private int nextLast;
     private int first;
     private int last;
 
     public ArrayDeque() {
         size = 8;
-        first = size / 2 - 1;
-        last = size / 2;
+        first = nextFirst = size / 2 - 1;
+        last = nextLast = size / 2;
         items = (T[]) new Object[size];
     }
 
     public void addFirst(T item) {
-        if (items[first] != null) {
+        if (items[nextFirst] != null) {
             // resize
         }
 
-        items[first] = item;
+        items[nextFirst] = item;
 
-        if (first == 0) {
-            first = size - 1;
+        if (nextFirst == 0) {
+            nextFirst = size - 1;
         } else  {
-            first -= 1;
+            nextFirst -= 1;
         }
     }
 
     public void addLast(T item) {
-        if (items[last] != null) {
-            // resize
+        if (items[nextLast] != null) {
+            resize();
         }
 
-        items[last] = item;
+        items[nextLast] = item;
 
-        if (last == size - 1) {
-            last = 0;
+        if (nextLast == size - 1) {
+            nextLast = 0;
         } else {
-            last += 1;
+            nextLast += 1;
+        }
+    }
+
+    public void resize() {
+        int newSize = size * 2;
+        T[] newItems = (T[]) new Object[size * 2];
+
+        int newFirst = newSize / 2 - 1;
+        int newNextFirst = newFirst;
+
+        int newLast = newSize / 2;
+        int newNextLast = newLast;
+
+        if (nextLast <= last) {
+            int noOfItemsToCopy = size / 2;
+            System.arraycopy(items, last, newItems, newNextLast, noOfItemsToCopy);
+            newNextLast += size / 2;
+            noOfItemsToCopy = nextLast;
+            System.arraycopy(items, 0, newItems, newNextLast, noOfItemsToCopy);
+            newNextLast += noOfItemsToCopy;
+        }
+
+        items = newItems;
+        size = newSize;
+
+        first = newFirst;
+        nextFirst = newNextFirst;
+
+        last = newLast;
+        nextLast = newNextLast;
+
+        if (nextLast >= size) {
+            nextLast = 0;
         }
     }
 
@@ -48,5 +83,9 @@ public class ArrayDeque<T> {
         AD.addLast(-1);
         AD.addLast(-3);
         AD.addLast(-6);
+        AD.addLast(-8);
+        AD.addLast(-11);
+        AD.addLast(-13);
+        AD.addLast(-22);
     }
 }
