60c8423af4767fefa767b71bd39a7b433ed4cf7c
zmmohibul <18-36427-1@student.aiub.edu>
2022-04-06 17:51:05 +0600

    Compareable - 1

---
+++ OOP/OurComparable/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Cat biral = new Cat("biral", 5);
+        Cat brishty = new Cat("brishty", 1);
+        Cat tapur = new Cat("tapur", 3);
+        Cat tupur = new Cat("tupur", 2);
+
+        Cat[] cats = new Cat[] { biral, brishty, tapur, tupur };
+        Cat maxCat = Cat.maxCat(cats);
+
+        System.out.println(maxCat);
+
+    }
+}
