80bd2741ae4a1af926e982df4da16ae5a5d368b9
zmmohibul <18-36427-1@student.aiub.edu>
2022-04-07 22:44:53 +0600

    BSTMap - Put

--- lab/lab7/BSTMap.java
+++ lab/lab7/BSTMap.java
@@ -7,9 +7,15 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>{
         V value;
         Node left;
         Node right;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
     }
 
     private Node root;
+    private int size;
 
     @Override
     public void clear() {
@@ -27,30 +33,49 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V>{
         return get(this.root, key);
     }
 
-    private V get(Node root, K key) {
-        if (root == null) {
+    private V get(Node node, K key) {
+        if (node == null) {
             return null;
         }
 
-        int cmp = root.key.compareTo(key);
+        int cmp = node.key.compareTo(key);
 
         if (cmp < 0) {
-            return get(root.left, key);
+            return get(node.left, key);
         } else if (cmp > 0) {
-            return get(root.right, key);
+            return get(node.right, key);
         } else {
-            return root.value;
+            return node.value;
         }
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
     @Override
     public void put(K key, V value) {
 
+    }
+
+    private Node put(Node node, K key, V value) {
+        if (node == null) {
+            return new Node(key, value);
+        }
+
+        int cmp = node.key.compareTo(key);
+        if (cmp < 0) {
+            node.left = put(node.left, key, value);
+        } else if (cmp > 0){
+            node.right = put(node.right, key, value);
+        } else {
+            node.value = value;
+        }
+
+        this.size += 1;
+
+        return new Node(key, value);
     }
 
     @Override
