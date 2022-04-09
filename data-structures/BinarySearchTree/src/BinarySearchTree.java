6fe82d159be16d1aa7f6929d54ca75cd8f933124
zmmohibul <18-36427-1@student.aiub.edu>
2022-04-09 15:37:12 +0600

    Binray Search Tree - insert

--- data-structures/BinarySearchTree/src/BinarySearchTree.java
+++ data-structures/BinarySearchTree/src/BinarySearchTree.java
@@ -3,11 +3,41 @@ public class BinarySearchTree<T extends Comparable<T>> {
         T value;
         Node left;
         Node right;
+
+        public Node(T value) {
+            this.value = value;
+            this.left = null;
+            this.right = null;
+        }
     }
 
     private Node root;
 
     public BinarySearchTree() {
         this.root = null;
+    }
+
+    public void insert(T value) {
+        if (root == null) {
+            root = new Node(value);
+            return;
+        }
+
+        insert(root, value);
+    }
+
+    private Node insert(Node node, T value) {
+        if (node == null) {
+            return new Node(value);
+        }
+
+        int cmp = value.compareTo(node.value);
+        if (cmp < 0) {
+            node.left = insert(node.left, value);
+        } else if (cmp > 0) {
+            node.right = insert(node.right, value);
+        }
+
+        return node;
     }
 }
---
+++ data-structures/BinarySearchTree/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        BinarySearchTree<Integer> tree = new BinarySearchTree<>();
+        tree.insert(10);
+        tree.insert(6);
+        tree.insert(15);
+        tree.insert(3);
+        tree.insert(8);
+        tree.insert(20);
+    }
+}
