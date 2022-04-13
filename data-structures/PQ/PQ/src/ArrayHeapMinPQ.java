1fdea2765593145197d92bf0337bd6192919c80f
zmmohibul <18-36427-1@student.aiub.edu>
2022-04-14 03:11:53 +0600

    MinPQ - add

--- data-structures/PQ/PQ/src/ArrayHeapMinPQ.java
+++ data-structures/PQ/PQ/src/ArrayHeapMinPQ.java
@@ -1,5 +1,54 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayHeapMinPQ<T> {
 
+    private List<PriorityNode> items;
+
+    public ArrayHeapMinPQ() {
+        items = new ArrayList<>();
+    }
+
+    public void add(T item, double priority) {
+        PriorityNode n = new PriorityNode(item, priority);
+        items.add(n);
+
+        if (items.size() == 1) {
+            return;
+        }
+
+        swim(items.size() - 1);
+    }
+
+    public void print() {
+        for (PriorityNode item : items) {
+            System.out.print(item.item + ", ");
+        }
+        System.out.println();
+    }
+
+    private void swim(int n) {
+        int p = parent(n);
+
+        PriorityNode node = items.get(n);
+        PriorityNode parent = items.get(p);
+
+        if (node.getPriority() < parent.getPriority()) {
+            swap(n, p);
+            swim(p);
+        }
+
+    }
+
+    private void swap(int n, int p) {
+        PriorityNode temp = items.get(n);
+        items.set(n, items.get(p));
+        items.set(p, temp);
+    }
+
+    private int parent(int n) {
+        return (n - 1) / 2;
+    }
 
     private class PriorityNode implements Comparable<PriorityNode> {
         private T item;
@@ -34,5 +83,29 @@ public class ArrayHeapMinPQ<T> {
 
             return Double.compare(this.getPriority(), o.getPriority());
         }
+    }
+
+    public static void main(String[] args) {
+        ArrayHeapMinPQ<Integer> pq = new ArrayHeapMinPQ<>();
+
+        pq.add(1, 1);
+        pq.add(5, 5);
+        pq.add(1, 1);
+        pq.add(6, 6);
+        pq.add(5, 5);
+        pq.add(6, 6);
+        pq.add(3, 3);
+        pq.add(7, 7);
+        pq.add(7, 7);
+        pq.add(8, 8);
+
+        pq.print();
+
+        pq.add(3, 3);
+        pq.print();
+
+
+
+
     }
 }
